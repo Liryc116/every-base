6c8a86d2ac83a871796651a1b0293a55f06136d6
cyril <cyrblin@gmail.com>
2022-06-15 00:11:17 +0900

    added sieve of eratosthenes - done with JS

---
+++ src/js/sieve_of_eratosthenes.js
@@ -0,0 +1,19 @@
+function sieve_of_eratosthenes(n)
+{
+    n+=1; // So the highest index is n
+    let result = new Array(n);
+    for (let i = 0; i < n; i++) 
+        result[i] = true;
+    result[0] = result[1] = false;
+
+    for(let i = 2; i<n; i++)
+    {
+        if(result[i])
+        {
+            for(let j = i*i; j<n; j+=i)
+                result[j] = false;
+        }
+    }
+
+    return result;
+}
